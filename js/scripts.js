995e180106caa14fb33cdde557c1d00b498648c2
Jared Gorthy <jago2136@colorado.edu>
2020-12-16 21:50:01 -0700

    quick modification

--- js/scripts.js
+++ js/scripts.js
@@ -82,7 +82,7 @@
         $(window).scroll( function(){
         
             /* Check the location of each desired element */
-            $('.vtimeline-point').each( function(i){
+            $('.vtimeline-content').each( function(i){
                 
                 var bottom_of_object = $(this).position().top + $(this).outerHeight();
                 var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -90,7 +90,7 @@
                 /* If the object is completely visible in the window, fade it it */
                 if( bottom_of_window > bottom_of_object ){
                     
-                    $(this).animate({'opacity':'1'},4000);
+                    $(this).animate({'opacity':'1'},2500);
                         
                 }
                 
